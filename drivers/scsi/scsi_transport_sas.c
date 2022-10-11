6b528d49d223f13c06bd4b236749ecda0b940f34
John Garry <john.garry@huawei.com>
2022-10-11 11:46:22 +0100

    scsi: scsi_transport_sas: Alloc sdev for expander
    Allocate a scsi device for an expander for sending SMP commands on the
    expander sdev request queue.
    
    Use channel=1 to not conflict with scsi target devices.
    
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- drivers/scsi/scsi_transport_sas.c
+++ drivers/scsi/scsi_transport_sas.c
@@ -1553,6 +1553,11 @@ int sas_rphy_add(struct sas_rphy *rphy)
 
 		scsi_scan_target(&rphy->dev, 0, rphy->scsi_target_id, lun,
 				 SCSI_SCAN_INITIAL);
+	} else if (identify->device_type == SAS_EDGE_EXPANDER_DEVICE ||
+		   identify->device_type == SAS_FANOUT_EXPANDER_DEVICE) {
+
+		if (!scsi_get_dev(&rphy->dev, 1, rphy->scsi_target_id, 0))
+			return -ENODEV;
 	}
 
 	return 0;
@@ -1632,6 +1637,7 @@ sas_rphy_remove(struct sas_rphy *rphy)
 	case SAS_EDGE_EXPANDER_DEVICE:
 	case SAS_FANOUT_EXPANDER_DEVICE:
 		sas_remove_children(dev);
+		scsi_remove_target(dev);
 		break;
 	default:
 		break;
