5b22ccd28c988455666a7840eb4d9022ddb65dd7
John Garry <john.g.garry@oracle.com>
2022-12-16 15:17:17 +0000

    make sdebug prints more pretty

--- drivers/scsi/scsi_debug.c
+++ drivers/scsi/scsi_debug.c
@@ -13,7 +13,7 @@
  */
 
 
-#define pr_fmt(fmt) KBUILD_MODNAME ":%s: " fmt, __func__
+#define pr_fmt(fmt) KBUILD_MODNAME " : %s: " fmt, __func__
 
 #include <linux/module.h>
 #include <linux/align.h>
