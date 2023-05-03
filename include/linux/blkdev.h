8dd4d3b20a5024b26bf8dbed896c2f118dae2f51
Prasad Singamsetty <prasad.singamsetty@oracle.com>
2023-05-03 18:38:07 +0000

    fs/bdev: Add atomic write support info to statx
    Extend statx system call to return additional info for atomic write support
    support if the specified file is a block device.
    
    Add initial support for a block device.
    
    Signed-off-by: Prasad Singamsetty <prasad.singamsetty@oracle.com>
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -1524,6 +1524,7 @@ int sync_blockdev_range(struct block_device *bdev, loff_t lstart, loff_t lend);
 int sync_blockdev_nowait(struct block_device *bdev);
 void sync_bdevs(bool wait);
 void bdev_statx_dioalign(struct inode *inode, struct kstat *stat);
+void bdev_statx_atomic(struct inode *inode, struct kstat *stat);
 void printk_all_partitions(void);
 int __init early_lookup_bdev(const char *pathname, dev_t *dev);
 #else
@@ -1542,6 +1543,9 @@ static inline void sync_bdevs(bool wait)
 {
 }
 static inline void bdev_statx_dioalign(struct inode *inode, struct kstat *stat)
+{
+}
+static inline void bdev_statx_atomic(struct inode *inode, struct kstat *stat)
 {
 }
 static inline void printk_all_partitions(void)
