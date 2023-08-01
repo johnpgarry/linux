44119aa7fa2a5e373743593ceac537193e8bee31
John Garry <john.g.garry@oracle.com>
2023-08-01 15:40:10 +0100

    truncate, create, delete, and write

--- fs-stress.c
+++ fs-stress.c
@@ -47,10 +47,10 @@ extern int open(char *, int);
 
 enum ACTION {
     ACTION_CREATE = 0,
-    ACTION_DELETE = 2,
-    ACTION_WRITE = 4,
-    ACTION_TRUNCATE = 11,
-    ACTION_COUNT
+    ACTION_DELETE = 3,
+    ACTION_WRITE = 5,
+    ACTION_TRUNCATE = 100,
+    ACTION_COUNT = ACTION_TRUNCATE + 2
 };
 
 enum ACTION get_random_action(void)
@@ -88,6 +88,7 @@ int create_file(char *dir_path, unsigned long long file_name_create_index)
    
     sprintf(file_name, "%s/file_%lld", dir_path, file_name_create_index);
 
+    printf("%s create file %s\n", __func__, file_name);
     fd = open(file_name, O_CREAT | O_WRONLY);
     if (fd < 0) {
 
@@ -176,35 +177,21 @@ ssize_t statx(int dfd, const char *filename, unsigned flags,
           unsigned int mask, struct statx *buffer)
 {
     int rc;
-    printf("%s dfd=%d filename=%s flags=%d mask=%d __NR_statx=%d\n", __func__, dfd, filename, flags, mask, __NR_statx);
+   // printf("%s dfd=%d filename=%s flags=%d mask=%d __NR_statx=%d\n", __func__, dfd, filename, flags, mask, __NR_statx);
     rc= syscall(__NR_statx, dfd, filename, flags, mask, buffer);
-    printf("%s2 dfd=%d filename=%s flags=%d mask=%d rc=%d\n", __func__, dfd, filename, flags, mask, rc);
+  //  printf("%s2 dfd=%d filename=%s flags=%d mask=%d rc=%d\n", __func__, dfd, filename, flags, mask, rc);
     
     return rc;
 }
 
-
-int write_to_file(char * const * dir_path, char **file_to_write_to, unsigned long file_count, unsigned long average_file_size)
+char *find_random_file_name(char * const * dir_path, unsigned long file_index)
 {
-    unsigned long file_index = rand() % file_count;
-    int fd;
+    char *fts_accpath = NULL;
+    char *dir_name = *dir_path;
     FTS* file_system = NULL;
     FTSENT *node    = NULL;
-    char *dir_name = *dir_path;
-    char           *fts_accpath = NULL;
     unsigned long count = 0;
-    struct statx stx_buffer;
-    int stx_atflag = AT_SYMLINK_NOFOLLOW;
-    int rc;
-    unsigned int stx_mask = STATX_ALL;
-    ssize_t size_written;
-    struct iovec iov[1];
-    int type_of_write;
-    ssize_t size_of_write;
-    int offset_of_write;
-    unsigned long average_file_size_sectors = average_file_size / 512;
 
-    printf("%s *dir_path=%s file_index=%ld file_count=%ld dir_name=%s\n", __func__, *dir_path, file_index, file_count, dir_name);
 
     while (1) {
         char *tmp;
@@ -251,8 +238,32 @@ int write_to_file(char * const * dir_path, char **file_to_write_to, unsigned lon
 found_file:
     fts_close(file_system);
     
+    return fts_accpath;
+}
+
+int write_to_file(char * const * dir_path, char **file_to_write_to, unsigned long file_count, unsigned long average_file_size_bytes)
+{
+    int fd;
+    char *dir_name = *dir_path;
+    char *fts_accpath;
+    struct statx stx_buffer;
+    int stx_atflag = AT_SYMLINK_NOFOLLOW;
+    int rc;
+    unsigned int stx_mask = STATX_ALL;
+    ssize_t size_written;
+    struct iovec iov[1];
+    int type_of_write;
+    ssize_t size_of_write;
+    int offset_of_write;
+    unsigned long average_file_size_sectors = average_file_size_bytes / 512;
+    unsigned long file_index = rand() % file_count;
+
+    printf("%s *dir_path=%s file_index=%ld file_count=%ld dir_name=%s average_file_size_sectors=%ld bytes=%ld\n",
+        __func__, *dir_path, file_index, file_count, dir_name, average_file_size_sectors, average_file_size_bytes);
+    
+    fts_accpath = find_random_file_name(dir_path, file_index);
     if (fts_accpath == NULL) {
-        printf("%s3 fts_accpath=%s\n", __func__, fts_accpath);
+        printf("%s2 fts_accpath=%s\n", __func__, fts_accpath);
         return -1;
     }
 
@@ -260,75 +271,86 @@ found_file:
     //flags=256 mask=20479
     rc = statx(-100, fts_accpath, 256, 20479, &stx_buffer);
     if (rc) {
-      printf("statx(%s) = %d\n", fts_accpath, rc);
+      printf("%s4 statx(%s) = %d\n", __func__, fts_accpath, rc);
       return -1;
     }
 
-    printf("statx(%s) stx_size = %lld stx_blocks=%lld\n", fts_accpath, stx_buffer.stx_size, stx_buffer.stx_blocks);
+    printf("%s4 statx(%s) stx_size = %lld stx_blocks=%lld\n", __func__, fts_accpath, stx_buffer.stx_size, stx_buffer.stx_blocks);
 
     fd = open(fts_accpath, O_WRONLY);
     if (fd < 0) {
-        printf("%s4 could not open fts_accpath=%s fd=%d\n", __func__, fts_accpath, fd);
+        printf("%s5 could not open fts_accpath=%s fd=%d\n", __func__, fts_accpath, fd);
         return fd;
     }
 
-    type_of_write = rand() % 50;
 
-    printf("%s1 type_of_write=%d stx_blocks=%d\n", __func__,
-        type_of_write, stx_buffer.stx_blocks);
-    switch (type_of_write) {
-    case 21 ... 30:
-        // Re-write all current data
-        size_of_write = stx_buffer.stx_blocks;
+    printf("%s6 stx_blocks=%lld\n", __func__, stx_buffer.stx_blocks);
+
+restart:
+    if (stx_buffer.stx_blocks == 0) {
+        size_of_write = 1;
         offset_of_write = 0;
-        break;
-    case 31 ... 38:
-        // write from end of data
-        while (1) {
-            size_of_write = rand() % stx_buffer.stx_blocks;
-            size_of_write /= 10;
-            size_of_write++;
-            if (size_of_write > 0)
+        type_of_write = -1;
+    } else {
+        type_of_write = rand() % 50;
+        switch (type_of_write) {
+        case 21 ... 30:
+            // Re-write all current data
+            size_of_write = stx_buffer.stx_blocks;
+            offset_of_write = 0;
+            break;
+        case 31 ... 38:
+            // write from end of data
+            while (1) {
+                size_of_write = rand() % stx_buffer.stx_blocks;
+                size_of_write /= 10;
+                size_of_write++;
+                if (size_of_write > 0)
+                    break;
+            }
+            offset_of_write = stx_buffer.stx_blocks;
+            break;
+        case 39 ... 49:
+            // Extend file with a hole
+            // write from end of data
+            while (1) {
+                size_of_write = rand() % stx_buffer.stx_blocks;
+                size_of_write /= 10;
+                size_of_write++;
                 break;
-        }
-        offset_of_write = stx_buffer.stx_blocks;
-        break;
-    case 39 ... 49:
-        // Extend file with a hole
-        // write from end of data
-        while (1) {
-            size_of_write = rand() % stx_buffer.stx_blocks;
-            size_of_write /= 10;
-            size_of_write++;
+            }
+            while (1) {
+                offset_of_write = rand() % stx_buffer.stx_blocks;
+                offset_of_write /= 10;
+                offset_of_write++;
+                offset_of_write += stx_buffer.stx_blocks;
+                break;
+            }
+            break;
+        case 0 ... 20:
+        default:
+            // Just write a subset of current data
+            while (1) {
+                size_of_write = rand() % stx_buffer.stx_blocks;
+                if (size_of_write > 0)
+                    break;
+            }
+            
+            while (1) {
+                offset_of_write = rand() % stx_buffer.stx_blocks;
+                if (size_of_write + offset_of_write <= stx_buffer.stx_blocks)
+                    break;
+            }
             break;
         }
-        while (1) {
-            offset_of_write = rand() % stx_buffer.stx_blocks;
-            offset_of_write /= 10;
-            offset_of_write++;
-            offset_of_write += stx_buffer.stx_blocks;
-            break;
+
+        if (offset_of_write + size_of_write > average_file_size_sectors) {
+                goto restart;
         }
-        break;
-    case 0 ... 20:
-    default:
-        // Just write a subset of current data
-        while (1) {
-            size_of_write = rand() % stx_buffer.stx_blocks;
-            if (size_of_write > 0)
-                break;
-        }
-        
-        while (1) {
-            offset_of_write = rand() % stx_buffer.stx_blocks;
-            if (size_of_write + offset_of_write <= stx_buffer.stx_blocks)
-                break;
-        }
-        break;
     }
 
-    printf("%s2 writing size_of_write=%d offset_of_write=%d stx_blocks=%d\n", __func__,
-        size_of_write, offset_of_write, stx_buffer.stx_blocks);
+    printf("%s7 writing size_of_write=%zd offset_of_write=%d stx_blocks=%lld type_of_write=%d\n", __func__,
+        size_of_write, offset_of_write, stx_buffer.stx_blocks, type_of_write);
 
     size_of_write *= 512;
     offset_of_write *= 512;
@@ -338,7 +360,7 @@ found_file:
     iov[0].iov_base = malloc(iov[0].iov_len);
     if (!iov[0].iov_base) {
         close(fd);
-        printf("%s5 could not alloc buffer of size %d \n", __func__, iov[0].iov_len);
+        printf("%s8 could not alloc buffer of size %zd\n", __func__, iov[0].iov_len);
         return -1;
     }
 
@@ -347,7 +369,74 @@ found_file:
 
     close(fd);
     if (size_written != iov[0].iov_len) {
-        printf("%s6 wrote %zd, but should have written \n", __func__, size_written, iov[0].iov_len);
+        printf("%s9 wrote %zd, but should have written %zd\n", __func__, size_written, iov[0].iov_len);
+        return -1;
+    }
+
+    return 0;
+}
+
+
+int truncate_file(char * const * dir_path, char **file_to_truncate, unsigned long file_count)
+{
+    int file_to_delete_index = rand() % file_count;
+    char *fts_accpath;
+    struct statx stx_buffer;
+    int rc;
+    unsigned long new_size;
+
+    fts_accpath = find_random_file_name(dir_path, file_to_delete_index);
+    if (fts_accpath == NULL) {
+        printf("%s3 fts_accpath=%s\n", __func__, fts_accpath);
+        return -1;
+    }
+    *file_to_truncate = fts_accpath;
+
+
+    rc = statx(-100, fts_accpath, 256, 20479, &stx_buffer);
+    if (rc) {
+      printf("%s1 statx(%s) = %d failed\n", __func__, fts_accpath, rc);
+      return -1;
+    }
+    printf("%s statx(%s) stx_size = %lld stx_blocks=%lld\n", __func__, fts_accpath, stx_buffer.stx_size, stx_buffer.stx_blocks);
+    if (stx_buffer.stx_blocks == 0)
+        return 0;
+
+    new_size = rand() % stx_buffer.stx_blocks;
+
+    if (truncate(fts_accpath, new_size * 512)) {
+        printf("%s truncate failed for file %s\n", __func__, fts_accpath);
+        return -1;
+    }
+
+    rc = statx(-100, fts_accpath, 256, 20479, &stx_buffer);
+    if (rc) {
+      printf("%s2 statx(%s) = %d failed\n", __func__, fts_accpath, rc);
+      return -1;
+    }
+
+    if (stx_buffer.stx_blocks != new_size) {
+      printf("%s3 for %s, expected %lld blocks, got %ld blocks\n", __func__, fts_accpath, stx_buffer.stx_blocks, new_size);
+     // return -1;
+    }
+
+    return 0;
+}
+
+int delete_file(char * const * dir_path, char **file_to_delete, int file_count)
+{
+    int file_to_delete_index = rand() % file_count;
+    char *fts_accpath;
+    fts_accpath = find_random_file_name(dir_path, file_to_delete_index);
+    printf("%s delete file %s file_count=%d\n", __func__, fts_accpath, file_count);
+    if (fts_accpath == NULL) {
+        printf("%s3 fts_accpath=%s\n", __func__, fts_accpath);
+        return -1;
+    }
+    *file_to_delete = fts_accpath;
+
+    if (remove(fts_accpath)) {
+        printf("%s3 could not remove=%s\n", __func__, fts_accpath);
         return -1;
     }
 
@@ -368,12 +457,12 @@ int main(int argc, char* const argv[])
     unsigned long size;
     char read_buffer[PAGE_SIZE];
     int file_target_count;
-    unsigned long average_file_size;
+    unsigned long average_file_size_bytes;
     unsigned long file_count = 0;
     unsigned long long file_name_create_index = 0;
 
     if (argc < 4) {
-        printf("Usage: mount_dev dir_absolute_path file_target_count\n", argv[0]);
+        printf("Usage: mount_dev dir_absolute_path file_target_count\n");
         exit(255);
     }
 
@@ -388,13 +477,13 @@ int main(int argc, char* const argv[])
          printf("2 mount_dev=NULL\n");
         return -1;
     }
-    mount_dev = strstr(mount_dev, "/");
+    mount_dev = strstr(mount_dev + 1, "/");
     if (!mount_dev) {
          printf("3 mount_dev=NULL\n");
         return -1;
     }
-    sprintf(class_file, "/sys/class/block/%s/size", mount_dev);
-    fd = open("/sys/class/block/sda/size", O_RDONLY);
+    sprintf(class_file, "/sys/class/block/%s/size", mount_dev + 1);
+    fd = open(class_file, O_RDONLY);
     if (fd < 0) {
         printf("4 fd=%d for %s\n", fd, class_file);
         return fd;
@@ -408,7 +497,7 @@ int main(int argc, char* const argv[])
     printf("6 size=%ld sectors or %ld bytes\n", size, size * 512);
     size *= 512;
 
-    average_file_size = size / file_target_count;
+    average_file_size_bytes = size / file_target_count;
 
     fd = mkdir(dir_path, 0777);
     if (fd < 0) {
@@ -425,7 +514,7 @@ int main(int argc, char* const argv[])
 
     close(fd);
 
-    printf("7 average_file_size=%ld \n", average_file_size);
+    printf("7 average_file_size_bytes=%ld \n", average_file_size_bytes);
 
     while (1) {
         enum ACTION action = get_random_action();
@@ -445,9 +534,19 @@ int main(int argc, char* const argv[])
                 continue;
             }
             case ACTION_DELETE:
-            {
+            { 
+                int rc;
+                char *file_to_delete = NULL;
                 if (file_count == 0)
                     continue;
+                rc = delete_file(argv, &file_to_delete, file_count);
+                if (rc < 0) {
+                    printf("could not delete file %s, rc=%d\n", file_to_delete, rc);
+                    free(file_to_delete);
+                    exit(0);
+                }
+                file_count--;
+                free(file_to_delete);
                 continue;
             }
             case ACTION_WRITE:
@@ -455,17 +554,27 @@ int main(int argc, char* const argv[])
                 char *file_to_write_to = NULL;
                 if (file_count == 0)
                     continue;
-                int rc = write_to_file(argv, &file_to_write_to, file_count, average_file_size);
+                int rc = write_to_file(argv, &file_to_write_to, file_count, average_file_size_bytes);
                 if (rc < 0) {
                     printf("could not write to file %s, rc=%d\n", file_to_write_to, rc);
+                    free(file_to_write_to);
                     exit(0);
                 }
+                free(file_to_write_to);
                 continue;
             }
             case ACTION_TRUNCATE:
             {
+                char *file_to_truncate = NULL;
                 if (file_count == 0)
                     continue;
+                int rc = truncate_file(argv, &file_to_truncate, file_count);
+                if (rc < 0) {
+                    printf("could not %s, rc=%d\n", file_to_truncate, rc);
+                    free(file_to_truncate);
+                    exit(0);
+                }
+                free(file_to_truncate);
                 continue;
             }
             default: {
