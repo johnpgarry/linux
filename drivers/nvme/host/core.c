14036c0e3dc95696e56d43191d6d3a85745bef10
Alan Adamson <alan.adamson@oracle.com>
2023-09-29 10:27:26 +0000

    nvme: Support atomic writes
    Support reading atomic write registers to fill in request_queue
    properties.
    
    Use following method to calculate limits:
    atomic_write_max_bytes = flp2(NAWUPF ?: AWUPF)
    atomic_write_unit_min = logical_block_size
    atomic_write_unit_max = flp2(NAWUPF ?: AWUPF)
    atomic_write_boundary = NABSPF
    
    Signed-off-by: Alan Adamson <alan.adamson@oracle.com>
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -1882,6 +1882,7 @@ static void nvme_update_disk_info(struct gendisk *disk,
 		struct nvme_ns *ns, struct nvme_id_ns *id)
 {
 	sector_t capacity = nvme_lba_to_sect(ns, le64_to_cpu(id->nsze));
+	struct request_queue *q = disk->queue;
 	u32 bs = 1U << ns->lba_shift;
 	u32 atomic_bs, phys_bs, io_opt = 0;
 
@@ -1916,15 +1917,55 @@ static void nvme_update_disk_info(struct gendisk *disk,
 		io_opt = bs * (1 + le16_to_cpu(id->nows));
 	}
 
-	blk_queue_logical_block_size(disk->queue, bs);
+	blk_queue_logical_block_size(q, bs);
 	/*
 	 * Linux filesystems assume writing a single physical block is
 	 * an atomic operation. Hence limit the physical block size to the
 	 * value of the Atomic Write Unit Power Fail parameter.
 	 */
-	blk_queue_physical_block_size(disk->queue, min(phys_bs, atomic_bs));
-	blk_queue_io_min(disk->queue, phys_bs);
-	blk_queue_io_opt(disk->queue, io_opt);
+	blk_queue_physical_block_size(q, min(phys_bs, atomic_bs));
+	blk_queue_io_min(q, phys_bs);
+	blk_queue_io_opt(q, io_opt);
+
+	atomic_bs = rounddown_pow_of_two(atomic_bs);
+	if (id->nsfeat & NVME_NS_FEAT_ATOMICS && id->nawupf) {
+		if (id->nabo) {
+			dev_err(ns->ctrl->device, "Support atomic writes NABO=%d\n",
+				id->nabo);
+		} else {
+			u32 boundary;
+
+			if (le16_to_cpu(id->nabspf))
+				boundary = (le16_to_cpu(id->nabspf) + 1) * bs;
+			else
+				boundary = 0;
+
+			/*
+			 * The boundary size just needs to be a multiple
+			 * of atomic_bs (and not necessarily a power-of-2), so
+			 * this could be relaxed in future.
+			 */
+			if (!boundary || is_power_of_2(boundary)) {
+				blk_queue_atomic_write_max_bytes(q, atomic_bs);
+				blk_queue_atomic_write_unit_min_sectors(q,
+						bs >> SECTOR_SHIFT);
+				blk_queue_atomic_write_unit_max_sectors(q,
+						atomic_bs >> SECTOR_SHIFT);
+				blk_queue_atomic_write_boundary_bytes(q,
+						boundary);
+			} else {
+				dev_err(ns->ctrl->device, "Unsupported atomic write boundary (%d)\n",
+					boundary);
+			}
+		}
+	} else if (ns->ctrl->subsys->awupf) {
+		blk_queue_atomic_write_max_bytes(q, atomic_bs);
+		blk_queue_atomic_write_unit_min_sectors(q,
+				bs >> SECTOR_SHIFT);
+		blk_queue_atomic_write_unit_max_sectors(q,
+				atomic_bs >> SECTOR_SHIFT);
+		blk_queue_atomic_write_boundary_bytes(q, 0);
+	}
 
 	/*
 	 * Register a metadata profile for PI, or the plain non-integrity NVMe
@@ -1944,8 +1985,7 @@ static void nvme_update_disk_info(struct gendisk *disk,
 	set_capacity_and_notify(disk, capacity);
 
 	nvme_config_discard(disk, ns);
-	blk_queue_max_write_zeroes_sectors(disk->queue,
-					   ns->ctrl->max_zeroes_sectors);
+	blk_queue_max_write_zeroes_sectors(q, ns->ctrl->max_zeroes_sectors);
 }
 
 static bool nvme_ns_is_readonly(struct nvme_ns *ns, struct nvme_ns_info *info)
