kernelorg-mirror/linux_kernel_git_djwong_xfs-linux

xfs: check new rtbitmap records against rt refcount btree
When we're rebuilding the realtime bitmap, check the proposed free
extents against the rt refcount btree to make sure we don't commit any
grievous errors.

Signed-off-by: Darrick J. Wong <djwong@kernel.org>

fs/xfs/scrub/rtbitmap_repair.c
@@ -22,6 +22,7 @@
 #include "xfs_swapext.h"
 #include "xfs_rtbitmap.h"
 #include "xfs_rtgroup.h"
+#include "xfs_refcount.h"
 #include "scrub/scrub.h"
 #include "scrub/common.h"
 #include "scrub/trace.h"
@@ -447,6 +448,7 @@ xrep_rgbitmap_mark_free(
 	unsigned int		bufwsize;
 	xfs_extlen_t		mod;
 	xfs_rtword_t		mask;
+	enum xbtree_recpacking	outcome;
 	int			error;
 
 	if (!xfs_verify_rgbext(rtg, rb->next_rgbno, rgbno - rb->next_rgbno))
@@ -465,6 +467,25 @@ xrep_rgbitmap_mark_free(
 	nextrtx = xfs_rtb_to_rtx(mp, rtbno, &mod) + 1;
 	if (mod != mp->m_sb.sb_rextsize - 1)
 		return -EFSCORRUPTED;
+
+	/* Must not be shared or CoW staging. */
+	if (rb->sc->sr.refc_cur) {
+		error = xfs_refcount_has_records(rb->sc->sr.refc_cur,
+				XFS_REFC_DOMAIN_SHARED, rb->next_rgbno,
+				rgbno - rb->next_rgbno, &outcome);
+		if (error)
+			return error;
+		if (outcome != XBTREE_RECPACKING_EMPTY)
+			return -EFSCORRUPTED;
+
+		error = xfs_refcount_has_records(rb->sc->sr.refc_cur,
+				XFS_REFC_DOMAIN_COW, rb->next_rgbno,
+				rgbno - rb->next_rgbno, &outcome);
+		if (error)
+			return error;
+		if (outcome != XBTREE_RECPACKING_EMPTY)
+			return -EFSCORRUPTED;
+	}
 
 	trace_xrep_rgbitmap_record_free(mp, startrtx, nextrtx - 1);
 

johnpgarry/linux

fs/xfs/scrub/rtbitmap_repair.c
@@ -22,6 +22,7 @@
 #include "xfs_swapext.h"
 #include "xfs_rtbitmap.h"
 #include "xfs_rtgroup.h"
+#include "xfs_refcount.h"
 #include "scrub/scrub.h"
 #include "scrub/common.h"
 #include "scrub/trace.h"
@@ -418,7 +419,8 @@ xrep_rgbitmap_mark_free(
 	xfs_rgblock_t		rgbno)
 {
 	struct xfs_mount	*mp = rgb->sc->mp;
-	struct xfs_rtgroup	*rtg = rgb->sc->sr.rtg;
+	struct xchk_rt		*sr =  &rgb->sc->sr;
+	struct xfs_rtgroup	*rtg = sr->rtg;
 	xfs_rtblock_t		rtbno;
 	xfs_rtxnum_t		startrtx;
 	xfs_rtxnum_t		nextrtx;
@@ -427,6 +429,7 @@ xrep_rgbitmap_mark_free(
 	unsigned int		bufwsize;
 	xfs_extlen_t		mod;
 	xfs_rtword_t		mask;
+	enum xbtree_recpacking	outcome;
 	int			error;
 
 	if (!xfs_verify_rgbext(rtg, rgb->next_rgbno, rgbno - rgb->next_rgbno))
@@ -445,6 +448,25 @@ xrep_rgbitmap_mark_free(
 	nextrtx = xfs_rtb_to_rtxrem(mp, rtbno, &mod) + 1;
 	if (mod != mp->m_sb.sb_rextsize - 1)
 		return -EFSCORRUPTED;
+
+	/* Must not be shared or CoW staging. */
+	if (sr->refc_cur) {
+		error = xfs_refcount_has_records(sr->refc_cur,
+				XFS_REFC_DOMAIN_SHARED, rgb->next_rgbno,
+				rgbno - rgb->next_rgbno, &outcome);
+		if (error)
+			return error;
+		if (outcome != XBTREE_RECPACKING_EMPTY)
+			return -EFSCORRUPTED;
+
+		error = xfs_refcount_has_records(sr->refc_cur,
+				XFS_REFC_DOMAIN_COW, rgb->next_rgbno,
+				rgbno - rgb->next_rgbno, &outcome);
+		if (error)
+			return error;
+		if (outcome != XBTREE_RECPACKING_EMPTY)
+			return -EFSCORRUPTED;
+	}
 
 	trace_xrep_rgbitmap_record_free(mp, startrtx, nextrtx - 1);
 