8b434702f211124e77b6164b90eba7bdf65d73ce
John Garry <john.g.garry@oracle.com>
2023-11-03 13:44:21 +0000

    xfs_eof_alignment removal
    does not fix on its own

--- fs/xfs/xfs_iomap.c
+++ fs/xfs/xfs_iomap.c
@@ -185,7 +185,7 @@ xfs_eof_alignment(
 		 * If mounted with the "-o swalloc" option the alignment is
 		 * increased from the strip unit size to the stripe width.
 		 */
-		if (xfs_inode_force_align(ip)) {
+		if (0) {
 			align = xfs_get_extsz_hint(ip);
 			pr_err("%s xfs_inode_force_align align=%d\n", __func__, align);
 		} else if (mp->m_swidth && xfs_has_swalloc(mp))
