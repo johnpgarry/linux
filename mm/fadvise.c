3fac2ce0f266d02df9025dcb4d795637b5513b14
John Garry <john.g.garry@oracle.com>
2023-12-06 11:57:09 +0000

    relocate is_atomic_write_valid

--- mm/fadvise.c
+++ mm/fadvise.c
@@ -177,6 +177,27 @@ int generic_fadvise(struct file *file, loff_t offset, loff_t len, int advice)
 }
 EXPORT_SYMBOL(generic_fadvise);
 
+bool is_atomic_write_valid(unsigned int unit_min, unsigned int unit_max, loff_t pos, size_t length)
+{
+	pr_err("%s unit_min=%d unit_max=%d pos=%lld length=%zd\n", __func__, 
+		unit_min, unit_max, pos, length);
+	if (unlikely(!unit_min))
+		return false;
+	if (length & (unit_min - 1))
+		return false;
+	if (!is_power_of_2(length))
+		return false;
+	if (pos & (length - 1))
+		return false;
+	if (length > unit_max)
+		return false;
+//	WARN_ON_ONCE(1);
+	return true;
+
+}
+EXPORT_SYMBOL(is_atomic_write_valid);
+
+
 int vfs_fadvise(struct file *file, loff_t offset, loff_t len, int advice)
 {
 	if (file->f_op->fadvise)
