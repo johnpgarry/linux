f3cf0e651cef9f5b01a39626955daf2596aac35c
John Garry <john.g.garry@oracle.com>
2024-01-22 13:01:48 +0000

    fs: xfs: Support atomic write for statx
    Support providing info on atomic write unit min and max for an inode.
    
    For simplicity, currently we limit the min at the FS block size, but a
    lower limit could be supported in future. This is required by iomap
    DIO.
    
    The atomic write unit min and max is limited by the guaranteed extent
    alignment for the inode.
    
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- fs/xfs/xfs_iops.c
+++ fs/xfs/xfs_iops.c
@@ -546,6 +546,33 @@ xfs_stat_blksize(
 	return PAGE_SIZE;
 }
 
+static void
+xfs_get_atomic_write_attr(
+	struct xfs_inode	*ip,
+	unsigned int		*unit_min,
+	unsigned int		*unit_max)
+{
+	struct xfs_buftarg	*target = xfs_inode_buftarg(ip);
+	struct block_device	*bdev = target->bt_bdev;
+	struct request_queue	*q = bdev->bd_queue;
+	struct xfs_mount	*mp = ip->i_mount;
+	struct xfs_sb		*sbp = &mp->m_sb;
+	unsigned int		extsz_bytes = XFS_FSB_TO_B(mp, ip->i_extsize);
+
+	if (!xfs_inode_has_atomicwrites(ip)) {
+		*unit_min = 0;
+		*unit_max = 0;
+		return;
+	}
+
+	/* Floor at FS block size */
+	*unit_min = max(queue_atomic_write_unit_min_bytes(q),
+			sbp->sb_blocksize);
+	
+	*unit_max = min(queue_atomic_write_unit_max_bytes(q),
+			extsz_bytes);
+}
+
 STATIC int
 xfs_vn_getattr(
 	struct mnt_idmap	*idmap,
@@ -618,6 +645,13 @@ xfs_vn_getattr(
 			stat->result_mask |= STATX_DIOALIGN;
 			stat->dio_mem_align = bdev_dma_alignment(bdev) + 1;
 			stat->dio_offset_align = bdev_logical_block_size(bdev);
+		}
+		if (request_mask & STATX_WRITE_ATOMIC) {
+			unsigned int unit_min, unit_max;
+
+			xfs_get_atomic_write_attr(ip, &unit_min, &unit_max);
+			generic_fill_statx_atomic_writes(stat,
+				unit_min, unit_max);
 		}
 		fallthrough;
 	default:
