97281cb949820855ddb983bdfe44e76bcdcfcebe
John Garry <john.g.garry@oracle.com>
2024-01-22 13:23:32 +0000

    xfs: Support setting FMODE_CAN_ATOMIC_WRITE
    For when an inode is enabled for atomic writes, set FMODE_CAN_ATOMIC_WRITE
    flag. Only direct IO is currently supported, so check for that also.
    
    We rely on the block layer to reject atomic writes which exceed the bdev
    request_queue limits, so don't bother checking any such thing here.
    
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- fs/xfs/xfs_file.c
+++ fs/xfs/xfs_file.c
@@ -1172,6 +1172,18 @@ out_unlock:
 	return remapped > 0 ? remapped : ret;
 }
 
+static bool xfs_file_open_can_atomicwrite(
+	struct inode		*inode,
+	struct file		*file)
+{
+	struct xfs_inode	*ip = XFS_I(inode);
+
+	if (!(file->f_flags & O_DIRECT))
+		return false;
+
+	return xfs_inode_has_atomicwrites(ip);
+}
+
 STATIC int
 xfs_file_open(
 	struct inode	*inode,
@@ -1180,6 +1192,8 @@ xfs_file_open(
 	if (xfs_is_shutdown(XFS_M(inode->i_sb)))
 		return -EIO;
 	file->f_mode |= FMODE_NOWAIT | FMODE_CAN_ODIRECT;
+	if (xfs_file_open_can_atomicwrite(inode, file))
+		file->f_mode |= FMODE_CAN_ATOMIC_WRITE;
 	return generic_file_open(inode, file);
 }
 
