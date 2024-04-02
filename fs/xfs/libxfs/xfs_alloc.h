bf196a94a540d45f1de738af70a937eebe1f213f
Dave Chinner <dchinner@redhat.com>
2024-04-03 10:28:42 +1100

    xfs: simplify extent allocation alignment
    We currently align extent allocation to stripe unit or stripe width.
    That is specified by an external parameter to the allocation code,
    which then manipulates the xfs_alloc_args alignment configuration in
    interesting ways.
    
    The args->alignment field specifies extent start alignment, but
    because we may be attempting non-aligned allocation first there are
    also slop variables that allow for those allocation attempts to
    account for aligned allocation if they fail.
    
    This gets much more complex as we introduce forced allocation
    alignment, where extent size hints are used to generate the extent
    start alignment. extent size hints currently only affect extent
    lengths (via args->prod and args->mod) and so with this change we
    will have two different start alignment conditions.
    
    Avoid this complexity by always using args->alignment to indicate
    extent start alignment, and always using args->prod/mod to indicate
    extent length adjustment.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    jpg: fixup alignslop references in xfs_trace.h
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- fs/xfs/libxfs/xfs_alloc.h
+++ fs/xfs/libxfs/xfs_alloc.h
@@ -46,7 +46,7 @@ typedef struct xfs_alloc_arg {
 	xfs_extlen_t	minleft;	/* min blocks must be left after us */
 	xfs_extlen_t	total;		/* total blocks needed in xaction */
 	xfs_extlen_t	alignment;	/* align answer to multiple of this */
-	xfs_extlen_t	minalignslop;	/* slop for minlen+alignment calcs */
+	xfs_extlen_t	alignslop;	/* slop for alignment calcs */
 	xfs_agblock_t	min_agbno;	/* set an agbno range for NEAR allocs */
 	xfs_agblock_t	max_agbno;	/* ... */
 	xfs_extlen_t	len;		/* output: actual size of extent */
