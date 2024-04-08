14ad3ee28e4995e55cac13c7af6375a7a28a0799
John Garry <john.g.garry@oracle.com>
2024-04-08 10:06:33 +0100

    warn in sd_setup_read_write_cmnd

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -1355,6 +1355,8 @@ static blk_status_t sd_setup_read_write_cmnd(struct scsi_cmnd *cmd)
 	else
 		protect = 0;
 
+	WARN_ON(nr_blocks == 16384 / 512);
+
 	if (protect && sdkp->protection_type == T10_PI_TYPE2_PROTECTION) {
 		ret = sd_setup_rw32_cmnd(cmd, write, lba, nr_blocks,
 					 protect | fua, dld);
