3f2afe833ff30f2bbb4182a1da00fb47438e9ba4
John Garry <john.g.garry@oracle.com>
2024-06-03 13:19:09 +0100

    xfs: Use extent size granularity for iomap->io_block_size
    Currently iomap->io_block_size is set to the i_blocksize() value for the
    inode.
    
    Expand the sub-fs block size zering to now cover RT extents, by calling
    setting iomap->io_block_size as xfs_inode_alloc_unitsize().
    
    In xfs_iomap_write_unwritten(), update the unwritten range fsb to cover
    this extent granularity.
    
    In xfs_file_dio_write(), handle a write which is not aligned to extent
    size granularity as unaligned. Since the extent size granularity need not
    be a power-of-2, handle this also.
    
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -3107,17 +3107,22 @@ xfs_break_layouts(
 	return error;
 }
 
+unsigned int
+xfs_inode_alloc_unitsize_fsb(
+	struct xfs_inode	*ip)
+{
+	if (XFS_IS_REALTIME_INODE(ip))
+		return ip->i_mount->m_sb.sb_rextsize;
+
+	return 1;
+}
+
 /* Returns the size of fundamental allocation unit for a file, in bytes. */
 unsigned int
 xfs_inode_alloc_unitsize(
 	struct xfs_inode	*ip)
 {
-	unsigned int		blocks = 1;
-
-	if (XFS_IS_REALTIME_INODE(ip))
-		blocks = ip->i_mount->m_sb.sb_rextsize;
-
-	return XFS_FSB_TO_B(ip->i_mount, blocks);
+	return XFS_FSB_TO_B(ip->i_mount, xfs_inode_alloc_unitsize_fsb(ip));
 }
 
 /* Should we always be using copy on write for file writes? */
--- fs/xfs/xfs_inode.h
+++ fs/xfs/xfs_inode.h
@@ -630,6 +630,7 @@ int xfs_inode_reload_unlinked(struct xfs_inode *ip);
 bool xfs_ifork_zapped(const struct xfs_inode *ip, int whichfork);
 void xfs_inode_count_blocks(struct xfs_trans *tp, struct xfs_inode *ip,
 		xfs_filblks_t *dblocks, xfs_filblks_t *rblocks);
+unsigned int xfs_inode_alloc_unitsize_fsb(struct xfs_inode *ip);
 unsigned int xfs_inode_alloc_unitsize(struct xfs_inode *ip);
 
 int xfs_icreate_dqalloc(const struct xfs_icreate_args *args,
