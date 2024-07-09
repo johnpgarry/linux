a79ba5adb8f773c40c6ed359a8395fd0d9c80687
John Garry <john.g.garry@oracle.com>
2024-07-09 10:51:54 +0100

    block: Catch possible entries missing from hctx_flag_name[]
    Re-arrange members in hctx_flag_name[] to match the enum in which the flags
    are declared.
    
    Also add a BUILD_BUG_ON() call to ensure that we are not missing entries
    in hctx_flag_name[].
    
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- include/linux/blk-mq.h
+++ include/linux/blk-mq.h
@@ -644,6 +644,7 @@ struct blk_mq_ops {
 #endif
 };
 
+/* Keep hctx_flag_name[] in sync with the definitions below */
 enum {
 	BLK_MQ_F_SHOULD_MERGE	= 1 << 0,
 	BLK_MQ_F_TAG_QUEUE_SHARED = 1 << 1,
@@ -653,9 +654,12 @@ enum {
 	 */
 	BLK_MQ_F_STACKING	= 1 << 2,
 	BLK_MQ_F_TAG_HCTX_SHARED = 1 << 3,
-	BLK_MQ_F_BLOCKING	= 1 << 5,
+	BLK_MQ_F_BLOCKING	= 1 << 4,
 	/* Do not allow an I/O scheduler to be configured. */
-	BLK_MQ_F_NO_SCHED	= 1 << 6,
+	BLK_MQ_F_NO_SCHED	= 1 << 5,
+
+	BLK_MQ_F_MAX		= 6,
+
 	/*
 	 * Select 'none' during queue registration in case of a single hwq
 	 * or shared hwqs instead of 'mq-deadline'.
