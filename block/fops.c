b9e4dab5e8005584f8e9378a141740b12a36d3f1
John Garry <john.g.garry@oracle.com>
2024-08-02 11:45:27 +0100

    debug raid0
    # Conflicts:
    #	block/blk-merge.c
    #	block/blk-settings.c
    #	block/fops.c
    #	drivers/scsi/scsi_debug.c

--- block/fops.c
+++ block/fops.c
@@ -44,6 +44,7 @@ static bool blkdev_dio_invalid(struct block_device *bdev, loff_t pos,
 		!bdev_iter_is_aligned(bdev, iter);
 }
 
+
 #define DIO_INLINE_BIO_VECS 4
 
 static ssize_t __blkdev_direct_IO_simple(struct kiocb *iocb,
