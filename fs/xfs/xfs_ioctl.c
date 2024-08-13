c022c10c55b3644de91f7bfcfca3cf575382c3b4
Darrick J. Wong <djwong@kernel.org>
2024-08-13 16:36:31 +0000

    xfs: Introduce FORCEALIGN inode flag
    Add a new inode flag to require that all file data extent mappings must
    be aligned (both the file offset range and the allocated space itself)
    to the extent size hint.  Having a separate COW extent size hint is no
    longer allowed.
    
    The goal here is to enable sysadmins and users to mandate that all space
    mappings in a file must have a startoff/blockcount that are aligned to
    (say) a 2MB alignment and that the startblock/blockcount will follow the
    same alignment.
    
    Allocated space will be aligned to start of the AG, and not necessarily
    aligned with disk blocks. The upcoming atomic writes feature will rely and
    forcealign and will also require allocated space will also be aligned to
    disk blocks.
    
    reflink will not be supported for forcealign yet, so disallow a mount under
    this condition. This is because we have the limitation of pageache
    writeback not knowing how to writeback an entire allocation unut, so
    reject a mount with relink.
    
    RT vol will not be supported for forcealign yet, so disallow a mount under
    this condition. It will be possible to support RT vol and forcealign in
    future. For this, the inode extsize must be a multiple of rtextsize - this
    is enforced already in xfs_ioctl_setattr_check_extsize() and
    xfs_inode_validate_extsize().
    
    [jpg: many changes from orig, including forcealign inode verification
     rework, ioctl setattr rework disallow reflink a forcealign inode,
     disallow mount for forcealign + reflink or rt]
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>
    Co-developed-by: John Garry <john.g.garry@oracle.com>
    Reviewed-by: John Garry <john.g.garry@oracle.com>
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- fs/xfs/xfs_ioctl.c
+++ fs/xfs/xfs_ioctl.c
@@ -509,6 +509,39 @@ xfs_fileattr_get(
 	return 0;
 }
 
+/*
+ * Forcealign requires a non-zero extent size hint and a zero cow
+ * extent size hint.
+ */
+static int
+xfs_ioctl_setattr_forcealign(
+	struct xfs_inode	*ip,
+	struct fileattr		*fa)
+{
+	struct xfs_mount	*mp = ip->i_mount;
+
+	if (!xfs_has_forcealign(mp))
+		return -EINVAL;
+
+	if (xfs_is_reflink_inode(ip))
+		return -EINVAL;
+
+	if (!(fa->fsx_xflags & (FS_XFLAG_EXTSIZE |
+				FS_XFLAG_EXTSZINHERIT)))
+		return -EINVAL;
+
+	if (fa->fsx_xflags & FS_XFLAG_COWEXTSIZE)
+		return -EINVAL;
+
+	if (!fa->fsx_extsize)
+		return -EINVAL;
+
+	if (fa->fsx_cowextsize)
+		return -EINVAL;
+
+	return 0;
+}
+
 static int
 xfs_ioctl_setattr_xflags(
 	struct xfs_trans	*tp,
@@ -517,9 +550,12 @@ xfs_ioctl_setattr_xflags(
 {
 	struct xfs_mount	*mp = ip->i_mount;
 	bool			rtflag = (fa->fsx_xflags & FS_XFLAG_REALTIME);
+	bool			forcealign = fa->fsx_xflags & FS_XFLAG_FORCEALIGN;
 	uint64_t		i_flags2;
+	int			error;
 
-	if (rtflag != XFS_IS_REALTIME_INODE(ip)) {
+	if (rtflag != XFS_IS_REALTIME_INODE(ip) ||
+	    forcealign != xfs_inode_has_forcealign(ip)) {
 		/* Can't change realtime flag if any extents are allocated. */
 		if (xfs_inode_has_filedata(ip))
 			return -EINVAL;
@@ -551,6 +587,12 @@ xfs_ioctl_setattr_xflags(
 	i_flags2 = xfs_flags2diflags2(ip, fa->fsx_xflags);
 	if (i_flags2 && !xfs_has_v3inodes(mp))
 		return -EINVAL;
+
+	if (forcealign) {
+		error = xfs_ioctl_setattr_forcealign(ip, fa);
+		if (error)
+			return error;
+	}
 
 	ip->i_diflags = xfs_flags2diflags(ip, fa->fsx_xflags);
 	ip->i_diflags2 = i_flags2;
