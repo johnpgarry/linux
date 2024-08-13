3ee0b60df21b2f23fa49808f4e6be2c2c56f298c
Darrick J. Wong <djwong@kernel.org>
2024-08-13 16:36:31 +0000

    xfs: Introduce FORCEALIGN inode flag
    Add a new inode flag to require that all file data extent mappings must
    be aligned (both the file offset range and the allocated space itself)
    to the extent size hint.  Having a separate COW extent size hint is no
    longer allowed.
    
    The goal here is to enable sysadmins and users to mandate that all space
    mappings in a file must have a startoff/blockcount that are aligned to
    (say) a 2MB alignment and that the startblock/blockcount will follow the
    same alignment.
    
    Allocated space will be aligned to start of the AG, and not necessarily
    aligned with disk blocks. The upcoming atomic writes feature will rely and
    forcealign and will also require allocated space will also be aligned to
    disk blocks.
    
    reflink will not be supported for forcealign yet, so disallow a mount under
    this condition. This is because we have the limitation of pageache
    writeback not knowing how to writeback an entire allocation unut, so
    reject a mount with relink.
    
    RT vol will not be supported for forcealign yet, so disallow a mount under
    this condition. It will be possible to support RT vol and forcealign in
    future. For this, the inode extsize must be a multiple of rtextsize - this
    is enforced already in xfs_ioctl_setattr_check_extsize() and
    xfs_inode_validate_extsize().
    
    [jpg: many changes from orig, including forcealign inode verification
     rework, ioctl setattr rework disallow reflink a forcealign inode,
     disallow mount for forcealign + reflink or rt]
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>
    Co-developed-by: John Garry <john.g.garry@oracle.com>
    Reviewed-by: John Garry <john.g.garry@oracle.com>
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- fs/xfs/libxfs/xfs_inode_util.c
+++ fs/xfs/libxfs/xfs_inode_util.c
@@ -80,6 +80,8 @@ xfs_flags2diflags2(
 		di_flags2 |= XFS_DIFLAG2_DAX;
 	if (xflags & FS_XFLAG_COWEXTSIZE)
 		di_flags2 |= XFS_DIFLAG2_COWEXTSIZE;
+	if (xflags & FS_XFLAG_FORCEALIGN)
+		di_flags2 |= XFS_DIFLAG2_FORCEALIGN;
 
 	return di_flags2;
 }
@@ -126,6 +128,8 @@ xfs_ip2xflags(
 			flags |= FS_XFLAG_DAX;
 		if (ip->i_diflags2 & XFS_DIFLAG2_COWEXTSIZE)
 			flags |= FS_XFLAG_COWEXTSIZE;
+		if (ip->i_diflags2 & XFS_DIFLAG2_FORCEALIGN)
+			flags |= FS_XFLAG_FORCEALIGN;
 	}
 
 	if (xfs_inode_has_attr_fork(ip))
@@ -226,6 +230,8 @@ xfs_inode_inherit_flags2(
 		ip->i_diflags2 |= XFS_DIFLAG2_DAX;
 	if (xfs_is_metadir_inode(pip))
 		ip->i_diflags2 |= XFS_DIFLAG2_METADATA;
+	if (pip->i_diflags2 & XFS_DIFLAG2_FORCEALIGN)
+		ip->i_diflags2 |= XFS_DIFLAG2_FORCEALIGN;
 
 	/* Don't let invalid cowextsize hints propagate. */
 	failaddr = xfs_inode_validate_cowextsize(ip->i_mount, ip->i_cowextsize,
@@ -233,6 +239,14 @@ xfs_inode_inherit_flags2(
 	if (failaddr) {
 		ip->i_diflags2 &= ~XFS_DIFLAG2_COWEXTSIZE;
 		ip->i_cowextsize = 0;
+	}
+	if (ip->i_diflags2 & XFS_DIFLAG2_FORCEALIGN) {
+		failaddr = xfs_inode_validate_forcealign(ip->i_mount,
+				ip->i_extsize, ip->i_cowextsize,
+				VFS_I(ip)->i_mode, ip->i_diflags,
+				ip->i_diflags2);
+		if (failaddr)
+			ip->i_diflags2 &= ~XFS_DIFLAG2_FORCEALIGN;
 	}
 }
 
