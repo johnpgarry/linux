4249c3d763f319154f5c327236ac96cf2dff5109
John Garry <john.g.garry@oracle.com>
2024-11-12 09:40:51 +0000

    scsi_debug: DEF_ATOMIC_WR=1 and DEF_NUM_HOST and 350Mb DEV
    # Conflicts:
    #	drivers/scsi/scsi_debug.c

--- drivers/scsi/scsi_debug.c
+++ drivers/scsi/scsi_debug.c
@@ -111,7 +111,7 @@ static const char *sdebug_version_date = "20210520";
 #define ACK_NAK_TO 0x3
 
 /* Default values for driver parameters */
-#define DEF_NUM_HOST   1
+#define DEF_NUM_HOST   4
 #define DEF_NUM_TGTS   1
 #define DEF_MAX_LUNS   1
 /* With these defaults, this driver will make 1 host with 1 target
@@ -121,7 +121,7 @@ static const char *sdebug_version_date = "20210520";
 #define DEF_CDB_LEN 10
 #define DEF_JDELAY   1		/* if > 0 unit is a jiffy */
 #define DEF_DEV_SIZE_PRE_INIT   0
-#define DEF_DEV_SIZE_MB   8
+#define DEF_DEV_SIZE_MB   350
 #define DEF_ZBC_DEV_SIZE_MB   128
 #define DEF_DIF 0
 #define DEF_DIX 0
@@ -155,7 +155,7 @@ static const char *sdebug_version_date = "20210520";
 #define DEF_VIRTUAL_GB   0
 #define DEF_VPD_USE_HOSTNO 1
 #define DEF_WRITESAME_LENGTH 0xFFFF
-#define DEF_ATOMIC_WR 0
+#define DEF_ATOMIC_WR 1
 #define DEF_ATOMIC_WR_MAX_LENGTH 8192
 #define DEF_ATOMIC_WR_ALIGN 2
 #define DEF_ATOMIC_WR_GRAN 2
