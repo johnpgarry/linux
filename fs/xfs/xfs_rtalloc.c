8a2d054e75c522890b0d160d500a169b408399f6
John Garry <john.g.garry@oracle.com>
2024-11-28 16:42:56 +0000

    xfs: Add RT atomic write unit max to xfs_mount
    rtvol guarantees alloc unit alignment through rt_extsize. As such, it is
    possible to atomically write multiple FS blocks in a rtvol (up to
    rt_extsize).
    
    Add a member to xfs_mount to hold the pre-calculated atomic write unit max.
    
    The value in rt_extsize does not need to be a power-of-2, so find the
    largest power-of-2 evenly divisible into rt_extsize.
    
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -735,6 +735,30 @@ xfs_rtginode_ensure(
 	return xfs_rtginode_create(rtg, type, true);
 }
 
+void
+xfs_rt_awu_update(
+	struct xfs_mount	*mp)
+{
+	unsigned int		rsize = XFS_FSB_TO_B(mp, mp->m_sb.sb_rextsize);
+	unsigned int		awu_max;
+
+	if (is_power_of_2(rsize)) {
+		mp->m_rt_awu_max = rsize;
+		return;
+	}
+
+	/*
+	 * Find highest power-of-2 evenly divisible into sb_rextsize
+	 */
+	awu_max = mp->m_sb.sb_blocksize;
+	while (1) {
+		if (rsize % (awu_max * 2))
+			break;
+		awu_max *= 2;
+	}
+	mp->m_rt_awu_max = awu_max;
+}
+
 static struct xfs_mount *
 xfs_growfs_rt_alloc_fake_mount(
 	const struct xfs_mount	*mp,
@@ -969,6 +993,7 @@ xfs_growfs_rt_bmblock(
 	 */
 	mp->m_rsumlevels = nmp->m_rsumlevels;
 	mp->m_rsumblocks = nmp->m_rsumblocks;
+	mp->m_rt_awu_max = nmp->m_rt_awu_max;
 
 	/*
 	 * Recompute the growfsrt reservation from the new rsumsize.
