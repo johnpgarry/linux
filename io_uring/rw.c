5da553522fae1a9109cc06162ccf52730fec04a5
Keith Busch <kbusch@kernel.org>
2024-12-10 11:47:17 -0800

    io_uring: enable per-io write streams
    Allow userspace to pass a per-I/O write stream in the SQE:
    
          __u8 write_stream;
    
    The __u8 type matches the size the filesystems and block layer support.
    
    Application can query the supported values from the statx
    max_write_streams field. Unsupported values are ignored by file
    operations that do not support write streams or rejected with an error
    by those that support them.
    
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Keith Busch <kbusch@kernel.org>

--- io_uring/rw.c
+++ io_uring/rw.c
@@ -316,6 +316,7 @@ static int io_prep_rw(struct io_kiocb *req, const struct io_uring_sqe *sqe,
 	}
 	rw->kiocb.dio_complete = NULL;
 	rw->kiocb.ki_flags = 0;
+	rw->kiocb.ki_write_stream = READ_ONCE(sqe->write_stream);
 
 	rw->addr = READ_ONCE(sqe->addr);
 	rw->len = READ_ONCE(sqe->len);
