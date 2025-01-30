4f5cc648f17b59fb9c6cf3a2dec01bf7a6f79d97
John Garry <john.g.garry@oracle.com>
2025-01-30 13:35:54 +0000

    reduce debug
    Signed-off-by: John Garry <john.g.garry@oracle.com>
    
    # Conflicts:
    #	fs/xfs/xfs_iomap.c

--- fs/xfs/xfs_iops.c
+++ fs/xfs/xfs_iops.c
@@ -586,8 +586,8 @@ xfs_get_atomic_write_attr(
 		return;
 	}
 
-	pr_err("%s mp->awu_max=%d\n", __func__, mp->awu_max);
-	pr_err("%s1 XFS_FSB_TO_B(mp, mp->awu_max)=%lld (unsigned int)(XFS_FSB_TO_B(mp, mp->awu_max))=%d\n",
+	pr_err_once("%s mp->awu_max=%d\n", __func__, mp->awu_max);
+	pr_err_once("%s1 XFS_FSB_TO_B(mp, mp->awu_max)=%lld (unsigned int)(XFS_FSB_TO_B(mp, mp->awu_max))=%d\n",
 		__func__, XFS_FSB_TO_B(mp, mp->awu_max), (unsigned int)(XFS_FSB_TO_B(mp, mp->awu_max)));
 
 	*unit_min = ip->i_mount->m_sb.sb_blocksize;
