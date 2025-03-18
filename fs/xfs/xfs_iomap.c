c8d103af741032a1205cd9d1b88c0d64813fed6e
John Garry <john.g.garry@oracle.com>
2025-03-18 11:53:25 +0000

    Fixes for bdev and cow iomap missing nimaps

--- fs/xfs/xfs_iomap.c
+++ fs/xfs/xfs_iomap.c
@@ -1104,7 +1104,7 @@ xfs_atomic_write_cow_iomap_begin(
 		.br_state = XFS_EXT_UNWRITTEN,
 	};
 	struct xfs_bmbt_irec	cmap;
-	int			error;
+	int			nimaps = 1, error;
 	bool			shared = false;
 	unsigned int		lockmode = XFS_ILOCK_EXCL;
 	u64			seq;
