7655d6c0b613d50fd0617b5547b4a7d1ce0af2ca
John Garry <john.g.garry@oracle.com>
2025-07-07 09:36:53 +0100

    scsi debug enabled for raid atomic writes
    # Conflicts:
    #	drivers/scsi/scsi_debug.c
    
    # Conflicts:
    #	drivers/scsi/scsi_debug.c

--- drivers/scsi/scsi_debug.c
+++ drivers/scsi/scsi_debug.c
@@ -117,7 +117,7 @@ static const char *sdebug_version_date = "20210520";
 #define ACK_NAK_TO 0x3
 
 /* Default values for driver parameters */
-#define DEF_NUM_HOST   1
+#define DEF_NUM_HOST   4
 #define DEF_NUM_TGTS   1
 #define DEF_MAX_LUNS   1
 /* With these defaults, this driver will make 1 host with 1 target
@@ -127,11 +127,11 @@ static const char *sdebug_version_date = "20210520";
 #define DEF_CDB_LEN 10
 #define DEF_JDELAY   1		/* if > 0 unit is a jiffy */
 #define DEF_DEV_SIZE_PRE_INIT   0
-#define DEF_DEV_SIZE_MB   8
+#define DEF_DEV_SIZE_MB   100
 #define DEF_ZBC_DEV_SIZE_MB   128
 #define DEF_DIF 0
 #define DEF_DIX 0
-#define DEF_PER_HOST_STORE false
+#define DEF_PER_HOST_STORE true
 #define DEF_D_SENSE   0
 #define DEF_EVERY_NTH   0
 #define DEF_FAKE_RW	0
@@ -161,8 +161,8 @@ static const char *sdebug_version_date = "20210520";
 #define DEF_VIRTUAL_GB   0
 #define DEF_VPD_USE_HOSTNO 1
 #define DEF_WRITESAME_LENGTH 0xFFFF
-#define DEF_ATOMIC_WR 0
-#define DEF_ATOMIC_WR_MAX_LENGTH 128
+#define DEF_ATOMIC_WR 1
+#define DEF_ATOMIC_WR_MAX_LENGTH 64
 #define DEF_ATOMIC_WR_ALIGN 2
 #define DEF_ATOMIC_WR_GRAN 2
 #define DEF_ATOMIC_WR_MAX_LENGTH_BNDRY (DEF_ATOMIC_WR_MAX_LENGTH)
