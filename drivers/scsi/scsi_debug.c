4872f9dd1f0103083a6c6da53523532eda26c0e2
John Garry <john.g.garry@oracle.com>
2025-07-17 11:50:51 +0100

    scsi debug 400mb and atomics

--- drivers/scsi/scsi_debug.c
+++ drivers/scsi/scsi_debug.c
@@ -127,7 +127,7 @@ static const char *sdebug_version_date = "20210520";
 #define DEF_CDB_LEN 10
 #define DEF_JDELAY   1		/* if > 0 unit is a jiffy */
 #define DEF_DEV_SIZE_PRE_INIT   0
-#define DEF_DEV_SIZE_MB   8
+#define DEF_DEV_SIZE_MB   400
 #define DEF_ZBC_DEV_SIZE_MB   128
 #define DEF_DIF 0
 #define DEF_DIX 0
@@ -161,7 +161,7 @@ static const char *sdebug_version_date = "20210520";
 #define DEF_VIRTUAL_GB   0
 #define DEF_VPD_USE_HOSTNO 1
 #define DEF_WRITESAME_LENGTH 0xFFFF
-#define DEF_ATOMIC_WR 0
+#define DEF_ATOMIC_WR 1
 #define DEF_ATOMIC_WR_MAX_LENGTH 128
 #define DEF_ATOMIC_WR_ALIGN 2
 #define DEF_ATOMIC_WR_GRAN 2
