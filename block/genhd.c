0cb92da7b50d1f136ce48a5ce9d6ff50cd7c1e98
John Garry <john.g.garry@oracle.com>
2025-09-25 11:35:15 +0100

    Revert "hack to not scan partition"
    This reverts commit f7afb469a7fd302c837ec5b7b11b666c640ae30a.

--- block/genhd.c
+++ block/genhd.c
@@ -421,7 +421,8 @@ static void add_disk_final(struct gendisk *disk)
 		if (get_capacity(disk)) {
 			dev_err(ddev, "%s1.2 calling disk_scan_partitions disk->part0=%pS ddev=%pS ddev->devt=%d\n",
 				__func__, disk->part0, ddev, ddev->devt);
-			//disk_scan_partitions(disk, BLK_OPEN_READ);
+		if (get_capacity(disk))
+			disk_scan_partitions(disk, BLK_OPEN_READ);
 			dev_err(ddev, "%s1.3 called disk_scan_partitions disk->part0=%pS ddev=%pS ddev->devt=%d\n",
 				__func__, disk->part0, ddev, ddev->devt);
 		}
