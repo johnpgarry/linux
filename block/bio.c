ba16ef225cb534e7c8bbe254e0694e43c9b9c2c0
John Garry <john.g.garry@oracle.com>
2025-11-14 09:53:25 +0000

    reduce debug again

--- block/bio.c
+++ block/bio.c
@@ -1366,7 +1366,7 @@ int bio_iov_iter_get_pages(struct bio *bio, struct iov_iter *iter,
 
 static void submit_bio_wait_endio(struct bio *bio)
 {
-	pr_err("%s bio=%pS\n", __func__, bio);
+	pr_err_once("%s bio=%pS\n", __func__, bio);
 	complete(bio->bi_private);
 }
 
