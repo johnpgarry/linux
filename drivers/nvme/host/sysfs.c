377ebacf23fbf76bc6556f0fbfaa3fb0f13cdb56
John Garry <john.g.garry@oracle.com>
2025-12-03 12:48:17 +0000

    nvme work en progress

--- drivers/nvme/host/sysfs.c
+++ drivers/nvme/host/sysfs.c
@@ -204,7 +204,7 @@ static int ns_head_update_nuse(struct nvme_ns_head *head)
 		return 0;
 
 	srcu_idx = srcu_read_lock(&head->srcu);
-	ns = nvme_find_path(head);
+	ns = NULL;// fixme nvme_find_path(head);
 	if (!ns)
 		goto out_unlock;
 
