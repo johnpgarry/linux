c55e2282d4b5a6b0c64e059faab50f30744948ee
John Garry <john.g.garry@oracle.com>
2026-03-16 11:18:34 +0000

    scsi: alua: Add scsi_alua_stpg_run()
    Add a function to run stpg and handle error codes - it does equivalent
    handling as in alua_rtpg_work() from scsi_dh_alua.c
    
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- include/scsi/scsi_alua.h
+++ include/scsi/scsi_alua.h
@@ -30,6 +30,7 @@ int scsi_alua_sdev_init(struct scsi_device *sdev);
 void scsi_alua_sdev_exit(struct scsi_device *sdev);
 
 int scsi_alua_rtpg_run(struct scsi_device *sdev);
+int scsi_alua_stpg_run(struct scsi_device *sdev, bool optimize);
 
 int scsi_alua_init(void);
 void scsi_exit_alua(void);
@@ -38,6 +39,9 @@ void scsi_exit_alua(void);
 static inline int scsi_alua_rtpg_run(struct scsi_device *sdev)
 {
 	return 0;
+}
+static inline int scsi_alua_stpg_run(struct scsi_device *sdev, bool optimize)
+{
 }
 static inline int scsi_alua_sdev_init(struct scsi_device *sdev)
 {
