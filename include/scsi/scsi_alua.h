a882659cdc76bc533cf4e62e115a342c81d8a2b6
John Garry <john.g.garry@oracle.com>
2026-03-16 16:02:28 +0000

    scsi: core: Add implicit ALUA support
    For when no device handler is used, add ALUA support.
    
    This will be equivalent to when native SCSI multipathing is used.
    
    Essentially all the same handling is available as DH alua driver for
    rescan, request prep, sense handling.
    
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- include/scsi/scsi_alua.h
+++ include/scsi/scsi_alua.h
@@ -23,6 +23,7 @@ struct alua_data {
 	unsigned char		transition_tmo;
 	unsigned long		expiry;
 	unsigned long		interval;
+	struct delayed_work	work;
 	struct scsi_device	*sdev;
 	spinlock_t		lock;
 };
@@ -34,11 +35,15 @@ void scsi_alua_handle_state_transition(struct scsi_device *sdev);
 
 int scsi_alua_check_tpgs(struct scsi_device *sdev);
 
+enum scsi_disposition scsi_alua_check_sense(struct scsi_device *sdev,
+				struct scsi_sense_hdr *sense_hdr);
+
 int scsi_alua_rtpg_run(struct scsi_device *sdev);
 int scsi_alua_stpg_run(struct scsi_device *sdev, bool optimize);
 
 blk_status_t scsi_alua_prep_fn(struct scsi_device *sdev, struct request *req);
 
+void scsi_device_alua_rescan(struct scsi_device *sdev);
 bool scsi_device_alua_implicit(struct scsi_device *sdev);
 
 int scsi_alua_init(void);
@@ -52,6 +57,12 @@ static inline int scsi_alua_check_tpgs(struct scsi_device *sdev)
 {
 	return 0;
 }
+static inline
+enum scsi_disposition scsi_alua_check_sense(struct scsi_device *sdev,
+				struct scsi_sense_hdr *sense_hdr){
+{
+	return SCSI_RETURN_NOT_HANDLED;
+}
 static inline int scsi_alua_rtpg_run(struct scsi_device *sdev)
 {
 	return 0;
@@ -63,6 +74,9 @@ static inline
 blk_status_t scsi_alua_prep_fn(struct scsi_device *sdev, struct request *req)
 {
 	return BLK_STS_OK;
+}
+static inline void scsi_device_alua_rescan(struct scsi_device *sdev)
+{
 }
 static inline bool scsi_device_alua_implicit(struct scsi_device *sdev)
 {
