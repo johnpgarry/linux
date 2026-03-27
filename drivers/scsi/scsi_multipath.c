08d83b35b939e201e3719ac2d0120b40bd313867
John Garry <john.g.garry@oracle.com>
2026-03-27 11:27:10 +0000

    fix setting iopolicy for scsi

--- drivers/scsi/scsi_multipath.c
+++ drivers/scsi/scsi_multipath.c
@@ -527,6 +527,7 @@ static struct scsi_mpath_head *scsi_mpath_alloc_head(void)
 	scsi_mpath_head->mpath_head->mpdt = &smpdt_pr;
 	scsi_mpath_head->mpath_head->drvdata = scsi_mpath_head;
 	scsi_mpath_head->mpath_head->drv_module = THIS_MODULE;
+	scsi_mpath_head->iopolicy.iopolicy = iopolicy;
 
 	scsi_mpath_head->index = ida_alloc(&scsi_multipath_dev_ida, GFP_KERNEL);
 	if (scsi_mpath_head->index < 0)
