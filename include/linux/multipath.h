944959492c16a827d599faafba6e8af1e72e3d37
John Garry <john.g.garry@oracle.com>
2026-06-10 10:48:53 +0100

    libmultipath: Add path selection support
    Add code for path selection.
    
    NVMe ANA is abstracted into enum mpath_access_state. The motivation here is
    so that SCSI ALUA can be used. Callbacks .is_disabled, .is_optimized,
    .get_access_state are added to get the path access state.
    
    Path selection modes round-robin, NUMA, and queue-depth are added, same
    as NVMe supports.
    
    NVMe has almost like-for-like equivalents here:
    - __mpath_find_path() -> __nvme_find_path()
    - mpath_find_path() -> nvme_find_path()
    
    and similar for all introduced callee functions.
    
    Functions mpath_set_iopolicy() and mpath_get_iopolicy() are added for
    setting default iopolicy.
    
    A separate mpath_iopolicy structure is introduced. There is no iopolicy
    member included in the mpath_head structure as it may not suit NVMe, where
    iopolicy is per-subsystem and not per namespace.
    
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- include/linux/multipath.h
+++ include/linux/multipath.h
@@ -7,13 +7,36 @@
 
 extern const struct block_device_operations mpath_ops;
 
+enum mpath_iopolicy_e {
+	MPATH_IOPOLICY_NUMA,
+	MPATH_IOPOLICY_RR,
+	MPATH_IOPOLICY_QD,
+};
+
+struct mpath_iopolicy {
+	enum mpath_iopolicy_e	iopolicy;
+};
+
+enum mpath_access_state {
+	MPATH_STATE_OPTIMIZED,
+	MPATH_STATE_NONOPTIMIZED,
+	MPATH_STATE_OTHER
+};
+
 struct mpath_device {
 	struct mpath_head	*mpath_head;
 	struct list_head	siblings;
 	struct gendisk		*disk;
+	int			numa_node;
+	enum mpath_access_state access_state;
 };
 
 struct mpath_head_template {
+	bool (*is_disabled)(struct mpath_device *);
+	bool (*is_optimized)(struct mpath_device *);
+	int (*get_nr_active)(struct mpath_device *);
+	enum mpath_iopolicy_e (*get_iopolicy)(struct mpath_head *);
+	const struct attribute_group **device_groups;
 };
 
 #define MPATH_HEAD_DISK_LIVE 			0
@@ -44,6 +67,14 @@ static inline struct mpath_head *mpath_gendisk_to_head(struct gendisk *disk)
 	return mpath_bd_device_to_head(disk_to_dev(disk));
 }
 
+static inline enum mpath_iopolicy_e mpath_read_iopolicy(
+			struct mpath_iopolicy *mpath_iopolicy)
+{
+	return READ_ONCE(mpath_iopolicy->iopolicy);
+}
+void mpath_synchronize(struct mpath_head *mpath_head);
+int mpath_set_iopolicy(const char *str, enum mpath_iopolicy_e *iopolicy);
+int mpath_get_iopolicy(char *buf, int iopolicy);
 int mpath_get_head(struct mpath_head *mpath_head);
 void mpath_put_head(struct mpath_head *mpath_head);
 int mpath_head_init(struct mpath_head *mpath_head);
@@ -63,4 +94,10 @@ static inline bool is_mpath_disk(struct gendisk *disk)
 	return false;
 	#endif
 }
+
+static inline bool mpath_qd_iopolicy(struct mpath_iopolicy *mpath_iopolicy)
+{
+	return mpath_read_iopolicy(mpath_iopolicy) == MPATH_IOPOLICY_QD;
+}
+
 #endif // _LIBMULTIPATH_H
