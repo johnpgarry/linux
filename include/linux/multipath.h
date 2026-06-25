7f72f45f00f429e5ae9a90e7d7bed21aef641b6f
John Garry <john.g.garry@oracle.com>
2026-06-25 11:25:48 +0100

    libmultipath: Add basic gendisk support
    Add support to allocate and free a multipath gendisk.
    
    NVMe has almost like-for-like equivalents here:
    - mpath_alloc_head_disk() -> nvme_mpath_alloc_disk()
    - multipath_partition_scan_work() -> nvme_partition_scan_work()
    - mpath_remove_disk() -> nvme_remove_head()
    - mpath_device_set_live() -> nvme_mpath_set_live()
    
    struct mpath_head_template is introduced as a method for drivers to
    provide custom multipath functionality.
    
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- include/linux/multipath.h
+++ include/linux/multipath.h
@@ -5,10 +5,18 @@
 #include <linux/blkdev.h>
 #include <linux/srcu.h>
 
+extern const struct block_device_operations mpath_ops;
+
 struct mpath_device {
+	struct mpath_head	*mpath_head;
 	struct list_head	siblings;
 	struct gendisk		*disk;
 };
+
+struct mpath_head_template {
+};
+
+#define MPATH_HEAD_DISK_LIVE 			0
 
 struct mpath_head {
 	struct srcu_struct	srcu;
@@ -17,12 +25,42 @@ struct mpath_head {
 
 	refcount_t		refcount;
 
+	unsigned long		flags;
+	struct gendisk		*disk;
+	struct work_struct	partition_scan_work;
+	struct device		*parent;
+	const struct attribute_group 		**disk_groups;
+	const struct mpath_head_template	*mpdt;
 	struct mpath_device __rcu 		*current_path[MAX_NUMNODES];
 };
+
+static inline struct mpath_head *mpath_bd_device_to_head(struct device *dev)
+{
+	return dev_get_drvdata(dev);
+}
+
+static inline struct mpath_head *mpath_gendisk_to_head(struct gendisk *disk)
+{
+	return mpath_bd_device_to_head(disk_to_dev(disk));
+}
 
 int mpath_get_head(struct mpath_head *mpath_head);
 void mpath_put_head(struct mpath_head *mpath_head);
 int mpath_head_init(struct mpath_head *mpath_head);
 void mpath_head_uninit(struct mpath_head *mpath_head);
 
+void mpath_put_disk(struct mpath_head *mpath_head);
+void mpath_remove_disk(struct mpath_head *mpath_head);
+int mpath_alloc_head_disk(struct mpath_head *mpath_head,
+			struct queue_limits *lim, int numa_node);
+void mpath_device_set_live(struct mpath_device *mpath_device);
+
+static inline bool is_mpath_disk(struct gendisk *disk)
+{
+	#if IS_ENABLED(CONFIG_LIBMULTIPATH)
+	return disk->fops == &mpath_ops;
+	#else
+	return false;
+	#endif
+}
 #endif // _LIBMULTIPATH_H
