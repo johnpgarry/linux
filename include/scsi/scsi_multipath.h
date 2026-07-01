f9215ea183d2e8fb902c8ac49efa62fe402b8e93
John Garry <john.g.garry@oracle.com>
2026-07-01 19:03:26 +0100

    scsi-multipath: add scsi_mpath_{start,end}_request()
    Add scsi_mpath_{start,end}_request() to handle updating private multipath
    request data, like nvme_mpath_{start,end}_request().
    
    New member Scsi_Host.mpath_nr_active is added. This is required for queue
    depth multipath iopolicy.
    
    For NVMe, this count is per controller. The reason is that many NSes may
    be connected to a controller, so congestion should be judged at
    controller level.
    
    SCSI has no definition of a controller, but SCSI host is a comparable
    concept.
    
    Indeed, many SCSI disks may be connected to the same SCSI host, so it
    makes sense to count number of active requests at this point. However,
    for a transport like iSCSI Initiator over TCP/IP, we have a separate SCSI
    host per SCSI device (so there the count would be same at SCSI device
    level).
    
    
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- include/scsi/scsi_multipath.h
+++ include/scsi/scsi_multipath.h
@@ -55,6 +55,8 @@ void scsi_mpath_add_sysfs_link(struct scsi_device *sdev);
 void scsi_mpath_remove_sysfs_link(struct scsi_device *sdev);
 int scsi_mpath_get_head(struct scsi_mpath_head *scsi_mpath_head);
 void scsi_mpath_put_head(struct scsi_mpath_head *scsi_mpath_head);
+void scsi_mpath_start_request(struct request *req);
+void scsi_mpath_end_request(struct request *req);
 #else /* CONFIG_SCSI_MULTIPATH */
 
 struct scsi_mpath_head {
@@ -91,6 +93,12 @@ int scsi_mpath_get_head(struct scsi_mpath_head *scsi_mpath_head)
 }
 static inline
 void scsi_mpath_put_head(struct scsi_mpath_head *scsi_mpath_head)
+{
+}
+static inline void scsi_mpath_start_request(struct request *req)
+{
+}
+static inline void scsi_mpath_end_request(struct request *req)
 {
 }
 static inline void scsi_mpath_add_sysfs_link(struct scsi_device *sdev)
