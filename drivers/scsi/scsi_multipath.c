28120829c2e4fd19947473387bf3ad68fcc6aec3
John Garry <john.garry@linux.dev>
2026-07-29 10:51:01 +0000

    scsi-multipath: add delayed disk removal support
    Add support in core code for delayed disk removal support. In this, the
    callback calls into the scsi_driver to do the necessary removal work.
    
    The scsi_disk driver (sd) must ensure that the scsi_mpath_device does not
    go away while the delayed removal work is active, i.e. it must keep a
    reference.
    
    No reference to the scsi_disk multipath structures are kept outside that
    driver, so that driver needs to provide a scsi_driver.mpath_remove_head
    callback to do the necessary work.
    
    Signed-off-by: John Garry <john.garry@linux.dev>

--- drivers/scsi/scsi_multipath.c
+++ drivers/scsi/scsi_multipath.c
@@ -354,7 +354,34 @@ static bool scsi_mpath_available_path(struct mpath_device *mpath_device)
 	return false;
 }
 
+static int scsi_mpath_remove_head_drv(struct device_driver *drv, void *data)
+{
+	struct scsi_mpath_head *scsi_mpath_head = data;
+	struct scsi_driver *scsi_driver = to_scsi_driver(drv);
+
+	if (scsi_driver->mpath_remove_head)
+		scsi_driver->mpath_remove_head(scsi_mpath_head);
+
+	return 0;
+}
+
+static void scsi_mpath_remove_head_work(struct mpath_head *mpath_head)
+{
+	struct scsi_mpath_head *scsi_mpath_head = to_scsi_mpath_head(mpath_head);
+
+	/*
+	 * scsi_mpath_remove_head_drv() could drop the final reference to
+	 * scsi_mpath_head, meaning that we pass a free'd pointer further
+	 * drivers - take a reference so that won't happen.
+	 */
+	scsi_mpath_get_head(scsi_mpath_head);
+	bus_for_each_drv(&scsi_bus_type, NULL, scsi_mpath_head,
+		scsi_mpath_remove_head_drv);
+	scsi_mpath_put_head(scsi_mpath_head);
+}
+
 static struct mpath_head_template smpdt = {
+	.remove_head = scsi_mpath_remove_head_work,
 	.is_disabled = scsi_mpath_is_disabled,
 	.is_optimized = scsi_mpath_is_optimized,
 	.available_path = scsi_mpath_available_path,
